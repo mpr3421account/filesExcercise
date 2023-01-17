99bf9c842b34a6e2c45b3c702dc344a242142a2d
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-17 17:14:39 -0300

    Final

---
+++ filesExcercise/Entitites/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filesExcercise.Entitites
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue()
+        {
+            return Price * Quantity;
+        }
+    }
+}
--- filesExcercise/Program.cs
+++ filesExcercise/Program.cs
@@ -7,4 +7,37 @@ arquivo de origem, contendo apenas o nome e o valor total para
 aquele item (preço unitário multiplicado pela quantidade),
 conforme exemplo.
 */
+using filesExcercise.Entitites;
+using System.Globalization;
 
+Console.Write("Enter file full path: ");
+string sourceFilePath = Console.ReadLine();
+
+try
+{
+    string[] lines = File.ReadAllLines(sourceFilePath);
+    string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+    string targeFolderPath = sourceFolderPath + @"\out";
+    string targetFilePath = sourceFolderPath + @"\summary.csv";
+
+    Directory.CreateDirectory(targeFolderPath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePath))
+    {
+        foreach(string line in lines)
+        {
+            string[] fields = line.Split(',');
+            string name = fields[0];
+            double price = double.Parse(fields[1],CultureInfo.InvariantCulture);
+            int quantity = int.Parse(fields[2]);
+
+            Product prod = new Product(name,price,quantity);
+
+            sw.WriteLine(prod.Name + "," + prod.TotalValue().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("an error occurred: " + e.Message);
+}
